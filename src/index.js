556bbedce1312f07d32b18aae53be041c856a772
Scharaa <48009177+Scharaa@users.noreply.github.com>
2020-02-04 10:43:28 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import { render } from "react-dom";
 import Tree from "./Tree";
 import data from "./data";
 
-const App = () => <Tree data={data} width={1000} height={1000} />;
+
+const App = () => 
+<div>
+    The Attack Tree
+<Tree data={data} width={1000} height={1000} />
+</div>
+;
 
 render(<App />, document.getElementById("root"));
